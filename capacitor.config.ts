0ccc4326f2022fc606723dfdbe1d8cea2796b532
Bruno R. Holanda <brunorodriguesholanda@gmail.com>
2024-09-30 10:12:55 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Owphiyr Smart Home',
+  webDir: 'dist'
+};
+
+export default config;
